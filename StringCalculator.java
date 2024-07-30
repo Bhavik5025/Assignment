298e9b65f019529fd0ddd837e58c379bca934992
Bhavik Patel <bhavik5025@gmail.com>
2024-07-30 21:27:50 +0530

    StringCalculator Version : 2

--- StringCalculator.java
+++ StringCalculator.java
@@ -7,13 +7,16 @@ public class StringCalculator{
         }
 
         //convert string into string array using split
-        String[] numberStrings = numbers.split(",");
+        String[] numberStrings = numbers.split(",|\n");
 
-     
+        //initially number is zero
         int sum = 0;
         //in for loop convert string into integer using and add into sum using wrapper class 
         for (String numberString : numberStrings) {
-            sum += Integer.parseInt(numberString.trim());
+            //it is used to check number is not empty.
+            if (!numberString.trim().isEmpty()) {
+                sum += Integer.parseInt(numberString.trim());
+            }
         }
 
         return sum;
--- StringCalculatorTest.java
+++ StringCalculatorTest.java
@@ -4,11 +4,16 @@ public class StringCalculatorTest {
     public void testAdd() {
         StringCalculator calculator = new StringCalculator();
 
-        // Test cases Task 1
+        //Test Cases for string of comma-separated numbers,and any numbers
         Assert.assertEquals(15, calculator.add("1,2,3,4,5"));
         Assert.assertEquals(60, calculator.add("10,20,30"));
         Assert.assertEquals(0, calculator.add(""));
         Assert.assertEquals(1, calculator.add("1"));
         Assert.assertEquals(6, calculator.add("1,5"));
+
+        //Test cases for Handle new line Betweeen Numbers
+        Assert.assertEquals(6, calculator.add("1\n2,3")); 
+        Assert.assertEquals(10, calculator.add("1\n2\n3\n4")); 
+        Assert.assertEquals(6, calculator.add("1,\n2,3")); 
     }
 }
